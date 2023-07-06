8746ebda52afe7e59fa1b76e248a11b29a8ab4cf
zhangliang <2424123787@qq.com>
2023-07-06 21:12:56 +0800

    修改标题

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,7 +119,7 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>1Chat-AI绘画-MJ</div>
+        <div className={styles["sidebar-title"]}>1Chat-AI绘画-Midjourney</div>
         <div className={styles["sidebar-sub-title"]}>
           Your own AI assistant.
         </div>
